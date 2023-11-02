f9cdffd2efe2396ecf96abf792bafc42c5a0dc5c
Arnav Rawat <rawat.arnav@gmail.com>
2023-11-01 19:45:23 -0500

    Turn into query

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,48 +1,20 @@
--- return table of number of monitoring sites per state that is wanted, having more than some desired count (high data)
+-- return table of number of monitoring sites per state >= 5
 
-delimiter //
-create procedure StateMonitoringSites(in num int)
-    select State, count(SiteNum)
-    from Location
-    group by State
-    having count(SiteNum) >= num;
-end //
-delimiter ;
+select State, count(SiteNum)
+from Location
+group by State
+having count(SiteNum) >= 5;
 
--- Top 10 and bottom 10 (may be lower than 20)
-delimiter //
-create procedure TopBottom10 (in ccode int, in pollutantnum int)
-    (select City, County, Measured = case pollutantnum
-        when 0 then No2Mean
-        when 1 then O3Mean
-        when 2 then So2Mean
-        when 3 then CoMean
-    end
-    from Measurements natural join Location
-    where CityCode = ccode and
-    case pollutantnum
-        when 0 then No2Mean
-        when 1 then O3Mean
-        when 2 then So2Mean
-        when 3 then CoMean
-    end is not null
-    order by Measured desc
-    limit 10) union
-    (select City, County, Measured = case pollutantnum
-        when 0 then No2Mean
-        when 1 then O3Mean
-        when 2 then So2Mean
-        when 3 then CoMean
-    end
-    from Measurements natural join Location
-    where CityCode = ccode and
-    case pollutantnum
-        when 0 then No2Mean
-        when 1 then O3Mean
-        when 2 then So2Mean
-        when 3 then CoMean
-    end is not null
-    order by Measured asc
-    limit 10);
-end //
-delimiter ;
+-- Top 10 and bottom 10 NOMean (may be lower than 20 in count)
+
+((select City, County, NoMean
+from Measurements natural join Location
+where CityCode = ccode and NoMean is not null
+order by Measured desc
+limit 10)
+union
+(select City, County, NoMean
+from Measurements natural join Location
+where CityCode = ccode and NoMean is not null
+order by Measured asc
+limit 10) ) order by NoMean desc;
