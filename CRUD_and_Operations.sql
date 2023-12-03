6f3828c7e4b821aea23249724f957e81759557bf
aaronkstoss <aaronk6@illinois.edu>
2023-12-03 17:36:45 -0600

    Added CRUD_and_Operations and (work in progress) list of sql queries we'll need to fulfill our application functionalities

---
+++ CRUD_and_Operations.sql
@@ -0,0 +1,34 @@
+/*
+Here we'll write SQL queries that fulfill our functionality needs for the app, including CRUD operations,
+our advanced operation, and anything else that we'll call from the backend to act on the database
+*/
+
+-- CREATE new User -> when a user clicks "create new user" and enters appropriate fields ie:  Username, Password
+
+-- READ UserRole for a given user
+
+-- UPDATE UserRole -> done by admin only (need special ui for admin?)
+
+-- DELETE User -> done by admin only
+
+-- CREATE new pollutant record and insert into table -> done by users with "editor" privilege using special interface for insertion
+
+-- READ pollutant data records for a location -> when user queries a location in keyword search
+
+-- READ pollutant data records for a particular time/date range -> when user queries pollutant data by time/date, may be helpful for heatmap?
+
+-- CREATE Review -> when user clicks "write review" and types into textbox interface for a particular location
+
+-- READ Reviews by location -> return reviews for a particular location
+
+-- READ Reviews by User -> return all reviews by a particular User
+
+-- UPDATE Review -> when user chooses to edit a review they have left
+
+-- DELETE Review -> when user chooses to delete a review they have left
+
+-- CREATE SearchHistory -> when user writes a search in the search textbox
+
+-- READ SearchHistory -> when a user chooses to look at their SearchHistory
+
+-- Advanced Operation (?)
