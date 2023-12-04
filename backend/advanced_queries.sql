d8048871faebec086548e203d1fd589b4d2742b7
Arnav Rawat <rawat.arnav@gmail.com>
2023-12-04 16:45:38 -0600

    Create stored procedure containing two advanced queries and include skeleton for stored trigger

--- backend/advanced_queries.sql
+++ backend/advanced_queries.sql
@@ -30,54 +30,77 @@ DELIMITER ;
 
 -- Gets a pollutant score by comparing each pollutant with the distribution of all cities
 
-create procedure GetPollutantScore(in citycode varchar(25), in formattedDate varchar(10), out no2grade varchar(2), out o3grade varchar(2), out so2grade varchar(2), out cograde varchar(2))
+DELIMITER //
+create procedure GetPollutantScore(in formattedDate varchar(10), in cutoff varchar(2), out chosenCities varchar(5012))
 begin
     declare no2avg float; declare allno2avg float; declare allno2stddev float;
     declare o3avg float; declare allo3avg float; declare allo3stddev float;
     declare so2avg float; declare allso2avg float; declare allso2stddev float;
     declare coavg float; declare allcoavg float; declare allcostddev float;
+    declare CityTmp varchar(25);
 
-    create table CityAverages as
-    select City, avg(No2Mean) as cnavg, avg(O3Mean) as coavg, avg(So2Mean) as csavg, avg(CoMean) as ccavg
-    from (select SiteNum, City from Location) natural join Measurements
+    declare no2grade varchar(2);
+    declare o3grade varchar(2);
+    declare so2grade varchar(2);
+    declare cograde varchar(2);
+
+    declare done int default 0;
+    declare cityIter cursor for
+    select distinct SC.City, avg(No2Mean) as cnavg, avg(O3Mean) as coavg, avg(So2Mean) as csavg, avg(CoMean) as ccavg
+    from (select SiteNum, City from Location) as SC natural join Measurements
     where MeasureDate > formattedDate -- date format has lexographic ordering
-    group by City
-    having count(MeasurementID) > 12 or City = citycode; -- more than 12 distinct day-avg samples
+    group by SC.City
+    having count(MeasurementID) > 10; -- more than 12 distinct day-avg samples
+    declare continue handler for not found set done = 1;
 
     select avg(No2Mean), stddev_pop(No2Mean), avg(O3Mean), stddev_pop(O3Mean), avg(So2Mean), stddev_pop(So2Mean), avg(CoMean), stddev_pop(CoMean)
     into allno2avg, allno2stddev, allo3avg, allo3stddev, allso2avg, allso2stddev, allcoavg, allcostddev
-    from CityAverages;
+    from Location natural join Measurements
+    where MeasureDate > formattedDate and City in (select ci from Location natural join Measurements group by City having count(MeasurementId) > 10);
 
-    select cnavg, coavg, csavg, ccavg
-    into no2avg, o3avg, so2avg, coavg
-    from CityAverages
-    where City = citycode;
+    set chosenCities = "";
+    open cityIter;
+    repeat
+        fetch cityIter into CityTmp, no2avg, o3avg, so2avg, coavg;
 
-    if (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -1) then set no2grade = "C";
-    elseif (no2avg > allno2avg and (no2avg - allno2avg) / allno2stddev < 1) then set no2grade = "B";
-    elseif no2avg > allno2avg then set no2grade = "A";
-    elseif (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -2) then set no2grade = "D";
-    elseif (no2avg < allno2avg) then set no2grade = "F";
-    end if;
+        if (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -1) then set no2grade = "C";
+        elseif (no2avg > allno2avg and (no2avg - allno2avg) / allno2stddev < 1) then set no2grade = "B";
+        elseif no2avg > allno2avg then set no2grade = "A";
+        elseif (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -2) then set no2grade = "D";
+        elseif (no2avg < allno2avg) then set no2grade = "F";
+        end if;
 
-    if (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -1) then set o3grade = "C";
-    elseif (o3avg > allo3avg and (o3avg - allo3avg) / allo3stddev < 1) then set o3grade = "B";
-    elseif o3avg > allo3avg then set o3grade = "A";
-    elseif (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -2) then set o3grade = "D";
-    elseif (o3avg < allo3avg) then set o3grade = "F";
-    end if;
+        if (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -1) then set o3grade = "C";
+        elseif (o3avg > allo3avg and (o3avg - allo3avg) / allo3stddev < 1) then set o3grade = "B";
+        elseif o3avg > allo3avg then set o3grade = "A";
+        elseif (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -2) then set o3grade = "D";
+        elseif (o3avg < allo3avg) then set o3grade = "F";
+        end if;
 
-    if (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -1) then set so2grade = "C";
-    elseif (so2avg > allso2avg and (so2avg - allso2avg) / allso2stddev < 1) then set so2grade = "B";
-    elseif so2avg > allso2avg then set so2grade = "A";
-    elseif (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -2) then set so2grade = "D";
-    elseif (so2avg < allso2avg) then set so2grade = "F";
-    end if;
+        if (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -1) then set so2grade = "C";
+        elseif (so2avg > allso2avg and (so2avg - allso2avg) / allso2stddev < 1) then set so2grade = "B";
+        elseif so2avg > allso2avg then set so2grade = "A";
+        elseif (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -2) then set so2grade = "D";
+        elseif (so2avg < allso2avg) then set so2grade = "F";
+        end if;
 
-    if (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -1) then set cograde = "C";
-    elseif (coavg > allcoavg and (coavg - allcoavg) / allcostddev < 1) then set cograde = "B";
-    elseif coavg > allcoavg then set cograde = "A";
-    elseif (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -2) then set cograde = "D";
-    elseif (coavg < allcoavg) then set cograde = "F";
-    end if;
-end;
+        if (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -1) then set cograde = "C";
+        elseif (coavg > allcoavg and (coavg - allcoavg) / allcostddev < 1) then set cograde = "B";
+        elseif coavg > allcoavg then set cograde = "A";
+        elseif (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -2) then set cograde = "D";
+        elseif (coavg < allcoavg) then set cograde = "F";
+        end if;
+
+        if (cograde <= cutoff and so2grade <= cutoff and o3grade <= cutoff and no2grade <= cutoff)
+        then set chosenCities = concat(chosenCities, Citytemp, no2grade, o3grade, so2grade, cograde);
+        end if;
+    until done
+    end repeat;
+    close cityIter;
+end //
+DELIMITER ;
+
+-- create trigger PollutantSpike after insert
+-- on Measurements for each row
+--     declare no2avg float;
+--     if (new.No2Mean - no2avg)
