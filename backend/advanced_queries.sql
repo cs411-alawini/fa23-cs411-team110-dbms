834f97ed15fba46241cb2a750b3d2ab5c2fbe0b2
Arnav Rawat <rawat.arnav@gmail.com>
2023-12-04 20:34:20 -0600

    Modify backend slightly, final stored procedures

--- backend/advanced_queries.sql
+++ backend/advanced_queries.sql
@@ -2,35 +2,31 @@
 
 DELIMITER //
 create procedure UserQueries()
+begin
     SELECT sh.SearchID, sh.SearchQuery
     FROM SearchHistory sh natural join Users u
-    WHERE sh.UserID = (SELECT UserID FROM Users WHERE Username = ‘binchanrh’);
+    WHERE sh.UserID = (SELECT UserID FROM Users WHERE Username = 'binchanrh');
 end //
-DELIMITER ;
 
 -- Top 10 and bottom 10 NOMean (may be lower than 20 in count)
 
-
-DELIMITER //
 create procedure NoMean()
 begin
     ((select City, County, No2Mean
     from Measurements natural join Location
-    where City = “Phoenix” and No2Mean is not null
+    where City = "Phoenix" and No2Mean is not null
     order by No2Mean desc
     limit 10)
     union all
     (select City, County, No2Mean
     from Measurements natural join Location
-    where City = “Phoenix” and No2Mean is not null
+    where City = "Phoenix" and No2Mean is not null
     order by No2Mean
     limit 5) );
 end //
-DELIMITER ;
 
--- Gets a pollutant score by comparing each pollutant with the distribution of all cities
+-- Gets the cities (with sample size > 10) above a certain pollutant score, then returns them in a comma separated list: City,No2,o3,so2,co
 
-DELIMITER //
 create procedure GetPollutantScore(in formattedDate varchar(10), in cutoff varchar(2), out chosenCities varchar(5012))
 begin
     declare no2avg float; declare allno2avg float; declare allno2stddev float;
@@ -39,10 +35,10 @@ begin
     declare coavg float; declare allcoavg float; declare allcostddev float;
     declare CityTmp varchar(25);
 
-    declare no2grade varchar(2);
-    declare o3grade varchar(2);
-    declare so2grade varchar(2);
-    declare cograde varchar(2);
+    declare no2grade varchar(2) default "A";
+    declare o3grade varchar(2) default "A";
+    declare so2grade varchar(2) default "A";
+    declare cograde varchar(2) default "A";
 
     declare done int default 0;
     declare cityIter cursor for
@@ -56,49 +52,55 @@ begin
     select avg(No2Mean), stddev_pop(No2Mean), avg(O3Mean), stddev_pop(O3Mean), avg(So2Mean), stddev_pop(So2Mean), avg(CoMean), stddev_pop(CoMean)
     into allno2avg, allno2stddev, allo3avg, allo3stddev, allso2avg, allso2stddev, allcoavg, allcostddev
     from Location natural join Measurements
-    where MeasureDate > formattedDate and City in (select ci from Location natural join Measurements group by City having count(MeasurementId) > 10);
+    where MeasureDate > formattedDate and City in (select l.City from Location as l natural join Measurements group by l.City having count(MeasurementId) > 10);
 
-    set chosenCities = "";
+    set chosenCities = "a";
     open cityIter;
+    create table CityGrades (cname varchar(25), no2grade varchar(2), o3grade varchar(2), so2grade varchar(2), cograde varchar(2));
     repeat
         fetch cityIter into CityTmp, no2avg, o3avg, so2avg, coavg;
 
-        if (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -1) then set no2grade = "C";
+        if (no2avg < allno2avg) then set no2grade = "A";
         elseif (no2avg > allno2avg and (no2avg - allno2avg) / allno2stddev < 1) then set no2grade = "B";
-        elseif no2avg > allno2avg then set no2grade = "A";
-        elseif (no2avg < allno2avg and (no2avg - allno2avg) / allno2stddev > -2) then set no2grade = "D";
-        elseif (no2avg < allno2avg) then set no2grade = "F";
+        elseif (no2avg > allno2avg and (no2avg - allno2avg) / allno2stddev < 2) then set no2grade = "C";
+        elseif (no2avg > allno2avg) then set no2grade = "F";
+        else set no2grade = "A";
         end if;
 
-        if (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -1) then set o3grade = "C";
+        if (o3avg < allo3avg) then set o3grade = "A";
         elseif (o3avg > allo3avg and (o3avg - allo3avg) / allo3stddev < 1) then set o3grade = "B";
-        elseif o3avg > allo3avg then set o3grade = "A";
-        elseif (o3avg < allo3avg and (o3avg - allo3avg) / allo3stddev > -2) then set o3grade = "D";
-        elseif (o3avg < allo3avg) then set o3grade = "F";
+        elseif (o3avg > allo3avg and (o3avg - allo3avg) / allo3stddev > -2) then set o3grade = "C";
+        elseif (o3avg > allo3avg) then set o3grade = "F";
+        else set o3grade = "A";
         end if;
 
-        if (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -1) then set so2grade = "C";
+        if (so2avg < allso2avg) then set so2grade = "A";
         elseif (so2avg > allso2avg and (so2avg - allso2avg) / allso2stddev < 1) then set so2grade = "B";
-        elseif so2avg > allso2avg then set so2grade = "A";
-        elseif (so2avg < allso2avg and (so2avg - allso2avg) / allso2stddev > -2) then set so2grade = "D";
-        elseif (so2avg < allso2avg) then set so2grade = "F";
+        elseif (so2avg > allso2avg and (so2avg - allso2avg) / allso2stddev > -2) then set so2grade = "D";
+        elseif (so2avg > allso2avg) then set so2grade = "F";
+        else set so2avg = "A";
         end if;
 
-        if (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -1) then set cograde = "C";
+        if (coavg < allcoavg) then set cograde = "A";
         elseif (coavg > allcoavg and (coavg - allcoavg) / allcostddev < 1) then set cograde = "B";
-        elseif coavg > allcoavg then set cograde = "A";
-        elseif (coavg < allcoavg and (coavg - allcoavg) / allcostddev > -2) then set cograde = "D";
-        elseif (coavg < allcoavg) then set cograde = "F";
+        elseif (coavg > allcoavg and (coavg - allcoavg) / allcostddev > -2) then set cograde = "D";
+        elseif (coavg > allcoavg) then set cograde = "F";
+        else set cograde = "A";
         end if;
 
+        select cograde, so2grade, o3grade, no2grade;
         if (cograde <= cutoff and so2grade <= cutoff and o3grade <= cutoff and no2grade <= cutoff)
-        then set chosenCities = concat(chosenCities, Citytemp, no2grade, o3grade, so2grade, cograde);
+        then insert into CityGrades values (CityTmp, no2grade, o3grade, so2grade, cograde);
         end if;
     until done
     end repeat;
     close cityIter;
+
+    select * from CityGrades;
+    drop table CityGrades;
 end //
 DELIMITER ;
+
 
 -- create trigger PollutantSpike after insert
 -- on Measurements for each row
--- server.js
+++ server.js
@@ -11,37 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'dbms-sqlserver.us-central1.rdbms.googleapis.com',
-  user: 'root',
-  password: ":J%4Dc._\<RAznO'",
-  database: 'pollutantTracker',
+  host: '35.202.76.109',
+  user: 'smallpass',
+  password: "cloudsql01$%^",
+  database: 'pollutant_tracker',
 });
 
 db.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err);
+    console.log(":J%4Dc._\<RAznO\'");
   } else {
     console.log('Connected to the database');
-    executeSqlScripts();
   }
 });
-
-function executeSqlScripts() {
-  // Reads and executes SQL scripts
-  const sqlFolderPath = path.join(__dirname, 'backend'); 
-  fs.readdirSync(sqlFolderPath).forEach((file) => {
-    const sqlFilePath = path.join(sqlFolderPath, file);
-    const sql = fs.readFileSync(sqlFilePath, 'utf-8');
-
-    db.query(sql, (err) => {
-      if (err) {
-        console.error(`Error executing SQL script ${file}:`, err);
-      } else {
-        console.log(`SQL script ${file} executed successfully`);
-      }
-    });
-  });
-}
 
 app.get('/api/userQueries', (req, res) => {
   const userQueriesQuery = 'CALL UserQueries();';
@@ -59,22 +42,23 @@ app.get('/api/userQueries', (req, res) => {
 app.get('/api/noMean', (req, res) => {
   const noMeanQuery = 'CALL NoMean();';
 
-  db.query(noMeanQuery, (noMeanErr, noMeanResult) => {
+  db.query(noMeanQuery, (noMeanErr, noMeanResult, fields) => {
     if (noMeanErr) {
       console.error('Error executing NO2 mean query:', noMeanErr);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
+      console.log(fields)
       res.json(noMeanResult[0]);
     }
   });
 });
 
 app.get('/api/pollutantScore', (req, res) => {
-  const { cityCode, formattedDate } = req.query;
+  const { formattedDate, cutoff } = req.query;
 
   const pollutantScoreQuery = `
-    CALL GetPollutantScore('${cityCode}', '${formattedDate}', @no2grade, @o3grade, @so2grade, @cograde);
-    SELECT @no2grade AS no2grade, @o3grade AS o3grade, @so2grade AS so2grade, @cograde AS cograde;
+    CALL GetPollutantScore('${formattedDate}', '${cutoff}', @citygrades);
+    SELECT @citygrades as cityGrades;
   `;
 
   db.query(pollutantScoreQuery, (pollutantScoreErr, pollutantScoreResult) => {
