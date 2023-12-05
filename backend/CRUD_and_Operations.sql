d0df9bb3aee61dd2c3687106dc03233122645c3e
aaronkstoss <aaronk6@illinois.edu>
2023-12-04 23:45:16 -0600

    Changes to be committed: 	renamed:    CRUD_and_Operations.sql -> backend/CRUD_and_Operations.sql 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js to add userlookup

--- backend/CRUD_and_Operations.sql
+++ backend/CRUD_and_Operations.sql
@@ -28,6 +28,10 @@ select * from Measurements natural join Location where City = ?
 -- READ pollutant data records for a particular time/date range -> when user queries pollutant data by time/date, may be helpful for heatmap?
 select * from Measurements natural join Location where MeasureDate = ?
 
+-- Check for existence of username/password pair, if match return relevant info (username,password,userRole, if not return nothing)
+select userName
+
+
 -- CREATE Review -> when user clicks "write review" and types into textbox interface for a particular location
 insert into Reviews value(?,?,?,?,?,?)
 
@@ -50,3 +54,4 @@ insert into SearchHistory value(?,?,?)
 select * from SearchHistory where UserID = ?
 
 -- Advanced Operation (?)
+
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,49 +11,51 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: '35.202.76.109',
-  user: 'smallpass',
+  host: "35.202.76.109",
+  user: "smallpass",
   password: "cloudsql01$%^",
-  database: 'pollutant_tracker',
+  database: "pollutant_tracker",
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Database connection failed:', err);
-    console.log(":J%4Dc._\<RAznO\'");
+    console.error("Database connection failed:", err);
+    console.log(":J%4Dc._<RAznO'");
   } else {
-    console.log('Connected to the database');
+    console.log("Connected to the database");
   }
 });
 
-app.get('/api/userQueries', (req, res) => {
-  const userQueriesQuery = 'CALL UserQueries();';
+app.get("/api/userQueries", (req, res) => {
+  const userQueriesQuery = "CALL UserQueries();";
 
   db.query(userQueriesQuery, (userQueriesErr, userQueriesResult) => {
     if (userQueriesErr) {
-      console.error('Error executing user queries query:', userQueriesErr);
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error executing user queries query:", userQueriesErr);
+      res.status(500).json({ error: "Internal Server Error" });
     } else {
       res.json(userQueriesResult[0]);
     }
   });
 });
 
-app.get('/api/noMean', (req, res) => {
-  const noMeanQuery = 'CALL NoMean();';
+// Calls NoMean Stored Procedure
+app.get("/api/noMean", (req, res) => {
+  const noMeanQuery = "CALL NoMean();";
 
   db.query(noMeanQuery, (noMeanErr, noMeanResult, fields) => {
     if (noMeanErr) {
-      console.error('Error executing NO2 mean query:', noMeanErr);
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error executing NO2 mean query:", noMeanErr);
+      res.status(500).json({ error: "Internal Server Error" });
     } else {
-      console.log(fields)
+      console.log(fields);
       res.json(noMeanResult[0]);
     }
   });
 });
 
-app.get('/api/pollutantScore', (req, res) => {
+// Calls pollutantScore
+app.get("/api/pollutantScore", (req, res) => {
   const { formattedDate, cutoff } = req.query;
 
   const pollutantScoreQuery = `
@@ -63,13 +65,39 @@ app.get('/api/pollutantScore', (req, res) => {
 
   db.query(pollutantScoreQuery, (pollutantScoreErr, pollutantScoreResult) => {
     if (pollutantScoreErr) {
-      console.error('Error executing pollutant score query:', pollutantScoreErr);
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.error(
+        "Error executing pollutant score query:",
+        pollutantScoreErr
+      );
+      res.status(500).json({ error: "Internal Server Error" });
     } else {
       // Send the result as JSON response
       res.json(pollutantScoreResult[1][0]);
     }
   });
+});
+
+// Given login -> username and password, return successful or not
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body; // Extract properties 'username' and 'password' from request body
+
+  db.query(
+    "SELECT Username FROM Users WHERE Username = ? AND UserPassword = ?",
+    [username, password],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send("Internal server error");
+      }
+      // If empty we can assume username password combo is bad
+      if (results.length === 0) {
+        return res.status(401).send("Authentication failed");
+      }
+
+      // Otherwise successful
+      res.send("Authentication successful");
+    }
+  );
 });
 
 app.listen(port, () => {
