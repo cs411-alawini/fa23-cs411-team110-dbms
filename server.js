41d4f17874f50468ee513ba5dc1de77dfe8d62a7
aaronkstoss <aaronk6@illinois.edu>
2023-12-05 21:23:41 -0600

    Added update user role

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-const sqlFilePath = path.join(__dirname, 'backend', 'advanced_queries.sql');
-const sqlQuery = fs.readFileSync(sqlFilePath, 'utf-8');
+const sqlFilePath = path.join(__dirname, "backend", "advanced_queries.sql");
+const sqlQuery = fs.readFileSync(sqlFilePath, "utf-8");
 
 const db = mysql.createConnection({
   host: "35.202.76.109",
@@ -29,12 +29,11 @@ db.connect((err) => {
   }
 });
 
-
-app.get('/noMean', (req, res) => {
-  db.query('CALL NoMean()', (error, results) => {
+app.get("/noMean", (req, res) => {
+  db.query("CALL NoMean()", (error, results) => {
     if (error) {
-      console.error('Error calling NoMean stored procedure:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error calling NoMean stored procedure:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
     const mergedResults = results[0];
 
@@ -43,24 +42,28 @@ app.get('/noMean', (req, res) => {
 });
 
 app.get("/api/search", (req, res) => {
-  let {search: s} = req.query;
+  let { search: s } = req.query;
 
   if (s === undefined) {
-    return res.status(301).json({error: "Malformed query"})
+    return res.status(301).json({ error: "Malformed query" });
   }
 
-  s = s.padEnd(s.length + 1, '%')
-  s = s.padStart(s.length + 1, '%')
+  s = s.padEnd(s.length + 1, "%");
+  s = s.padStart(s.length + 1, "%");
 
-  db.query("SELECT City, avg(No2Mean) as No2Mean, avg(O3Mean) as O3Mean, avg(So2Mean) as So2Mean, avg(CoMean) as CoMean from Location natural join Measurements where City LIKE ? group by City;", [s], (qErr, qRes) => {
-    if (qErr) {
-      console.log(qErr)
-      return res.status(500).json({error: "Server Error"})
-    } else {
-      return res.status(200).json(qRes)
+  db.query(
+    "SELECT City, avg(No2Mean) as No2Mean, avg(O3Mean) as O3Mean, avg(So2Mean) as So2Mean, avg(CoMean) as CoMean from Location natural join Measurements where City LIKE ? group by City;",
+    [s],
+    (qErr, qRes) => {
+      if (qErr) {
+        console.log(qErr);
+        return res.status(500).json({ error: "Server Error" });
+      } else {
+        return res.status(200).json(qRes);
+      }
     }
-  })
-})
+  );
+});
 
 app.get("/api/userQueries", (req, res) => {
   const userQueriesQuery = "CALL UserQueries();";
@@ -77,10 +80,10 @@ app.get("/api/userQueries", (req, res) => {
 
 // Given login -> username and password, return successful or not (I used post instead of get, idk if that's best?)
 app.post("/api/login", (req, res) => {
-  const {username: u , password: p} = req.body; // Extract properties 'username' and 'password' from request body
+  const { username: u, password: p } = req.body; // Extract properties 'username' and 'password' from request body
 
   if (u === undefined || p === undefined) {
-    return res.status(400).json("Failed.")
+    return res.status(400).json("Failed.");
   }
 
   db.query(
@@ -104,7 +107,7 @@ app.post("/api/login", (req, res) => {
 
 // Read UserRole to decide whether to grant special user access (idk how this should be implemented on react side?)
 app.get("/api/user-role", (req, res) => {
-  const { username } = req.query; // Get from query
+  const { username: u, role: r } = req.query; // Get from query
   // Input validation
   if (!username) {
     return res.status(400).json({ error: "Username is required" });
@@ -127,7 +130,33 @@ app.get("/api/user-role", (req, res) => {
   );
 });
 
-app.get('/api/getPollutantScore', (req, res) => {
+// Update UserRole (for use by admin only)
+app.post("/api/update-role", (req, res) => {
+  const { username: u, role: r } = req.body; // Extract properties 'username' and 'role' from request body
+
+  if (u === undefined || r === undefined) {
+    return res.status(400).json("Failed.");
+  }
+
+  db.query(
+    "UPDATE Users SET UserRole = ? WHERE Users.Username = ?",
+    [r, u],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json("Internal server error");
+      }
+      if (results.length === 0) {
+        // Empty set returned
+        return res.status(404).json({ error: "Username not found" });
+      }
+      // Otherwise successful
+      res.json({ roleUpdate: " Update successful" });
+    }
+  );
+});
+
+app.get("/api/getPollutantScore", (req, res) => {
   const { formattedDate, cutoff } = req.query;
   const sql = `
     CALL GetPollutantScore('${formattedDate}', '${cutoff}');
@@ -135,8 +164,8 @@ app.get('/api/getPollutantScore', (req, res) => {
 
   db.query(sql, (error, results) => {
     if (error) {
-      console.error('Error calling GetPollutantScore stored procedure:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error calling GetPollutantScore stored procedure:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
     res.json(results);
   });
